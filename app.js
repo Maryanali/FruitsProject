1c028538df5f43fabee125dcd70697cf1ce0dd61
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-04-01 07:48:10 +0100

    Using mongoose & mongodb with node js

--- app.js
+++ app.js
@@ -1,57 +1,124 @@
 //jshint esversion:6
 
-const MongoClient  = require('mongodb').MongoClient;
-const assert = require('assert');
-// Replace the uri string with your MongoDB deployment's connection string.
-//this is the connection url
-const url= 'mongodb://localhost:27017';
+const mongoose  = require('mongoose');
+//make connection to mongodb server and looks for fruitsDB
+mongoose.connect("mongodb://localhost:27017/fruitDB",{ useNewUrlParser:true });
 
-const dbName= 'fruitsDB';
-//the mongo client connects to the mongodb database
-//if frutisdb does not exist, it creates a new one
-const client = new MongoClient(url, {useNewUrlParser: true});
-
-//use connect method to connect to the server
-
-client.connect(function(err){
- assert.equal(null,err);
- console.log("successfully connected to the server");
-
- const db =client.db(dbName);
-    findDocuments(db, function(){
- client.close();
-    });
+const fruitSchema =new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "name is required for data entry!"] 
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review: String
 });
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents(data) in key value pairs
-  collection.insertMany([
-    {name : "Apple", 
-    score : 8, 
-    review : "Great Fruit!"
-    }, 
-    {
-    name : "Orange", 
-    score : 5, 
-    review : "decent Fruit"
-    },{
-    name : "Pear", 
-    score : 2, 
-    review : "Basic Fruit"
-    }
-    //validation line
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
+//mongoose model
+const Fruit =mongoose.model("Fruit", fruitSchema);
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 10,
+  review: "Ok I reckon."
+});
 
-//how to find the documents
+//fruit.save();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitSchema //embedding a fruit document inside thedocuemt.
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const pineapple = new Fruit({
+  name: "Pineapple",
+  score: 9,
+  review: "great fruit"
+});
+
+const  person = new Person({
+  name: "Amy",
+  age: 37,
+  favouriteFruit: pineapple
+});
+const grapes = new Fruit({
+  name: "grapes",
+  score: 10,
+  review: "goes with everything"
+});
+
+const  person2 = new Person({
+  name: "John",
+  age: 30,
+  favouriteFruit: grapes
+});
+person2.save();
+person.save(); 
+
+const kiwi = new Fruit({
+  name: "kiwi",
+  score: 5,
+  review: "average"
+});
+const orange = new Fruit({
+  name: "grape friuit",
+  score: 10,
+  review: "delcious"
+});
+
+Fruit.insertMany([pineapple, kiwi, grapes], function(err){
+  if(err){
+    console.log(err);
+  } else{
+    console.log("success");
+  }
+});
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("successfully saved to db");
+//   }
+// });
+Fruit.find(function(err,fruits){
+  if (err){
+    console.log(err);
+  } else {
+    mongoose.connection.close();
+//loop through fruits array from database and console.log the fruits name.
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    });
+  }
+});
+// Fruit.updateOne({name:"grapes"},{name:"peach"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }else {
+//     console.log("Successfully updated fruit");
+//   }
+// });
+
+Fruit.deleteOne({name: "Apple"}, function(err){
+  if(err){
+    console.log(err);
+  }else {
+    console.log("Successfully deleted document");
+  }
+});
+  
+Person.deleteOne({name: "John", function(err){
+  if (err){
+    console.log(err);
+  } else {
+    console.log("successfully deleted people");
+  }
+}});
 
 const findDocuments = function(db, callback){
     //get the document collection
